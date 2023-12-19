3741304d5b0be34b815ac63c78a7d5eae6bd8727
momenurmaruf2742 <momenur.maruf@gmail.com>
2023-12-19 23:12:24 +0600

    banner images

--- category/models.py
+++ category/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from autoslug import AutoSlugField
 from django.urls import reverse
 
+#for image
+import os
+def category_banner_upload_to(instance,filename):
+    # Get the file extension
+    ext = filename.split('.')[-1]
+    # Generate a new file name using the category title
+    new_filename = f"{instance.title}_{filename}.{ext}"
+    # Return the complete file path
+    return os.path.join('photo/category', new_filename)
 
 class Category(models.Model):
     parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, blank = 
@@ -9,6 +18,8 @@ class Category(models.Model):
     title = models.CharField(max_length=100) 
     slug = models.SlugField(max_length=100,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    category_baner=models.ImageField(upload_to=category_banner_upload_to,null=True)
 
     def __str__(self):
         return self.title
