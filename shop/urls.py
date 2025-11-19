f57bc61a64a09f5cde5c8d67e2a3ba632d8e87fb
momenurmaruf2742 <momenur.maruf@gmail.com>
2025-11-19 22:39:11 +0600

    shop details page urls fix

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,5 @@ from . views import *
 
 urlpatterns = [
     path("", Shop.as_view(),name='shop'),
-    path('shop/<slug:category_slug>/<slug:product_slug>',product_detail,name='product_detail'),
+    path('<slug:category_slug>/<slug:product_slug>',product_detail,name='product_detail'),
 ]
